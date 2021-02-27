ca381de6a070877a1bdf7825f8a32b08cf35144c
matthewogtong <ogtongm@gmail.com>
2021-02-27 13:09:20 -0500

    Done.

--- src/reducer.js
+++ src/reducer.js
@@ -1,1 +1,30 @@
-// add code snippets from README
+let state 
+
+const reducer = (state = { count: 0 }, action) => {
+  switch (action.type) {
+    case "counter/increment":
+      return { count: state.count + 1 }
+    default:
+      return state
+  }
+}
+
+const dispatch = (action) => {
+    state = reducer(state, action)
+    render()
+}
+
+const render = () => {
+    let container = document.getElementById("container")
+    container.textContent = state.count
+}
+
+let button = document.getElementById("button")
+
+button.addEventListener("click", () => {
+    dispatch({ type: "counter/increment" })
+})
+
+dispatch({type: '@@INIT'})         
+
+
